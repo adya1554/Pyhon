ccf5c0c4c213639c59643342516227edfb2ebe19
intern <i@gmail.com>
2025-12-08 12:31:57 +0530

    comment

--- intern.sh
+++ intern.sh
@@ -1,4 +1,6 @@
 this is intern writing .sh file for shell scripting 
 ahil aau la job lagla 
 gunjan la job lagla 
-mi ektach rahilo............
+mi ektach rahilo...........ldfsbd
+d lndm; 
+f;knf .
