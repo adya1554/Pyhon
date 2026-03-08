a4131f26d4d395030629afebd3befa887c72e048
aditya <aditya@gmail.com>
2026-03-08 09:50:16 +0530

    string function program

---
+++ string.py
@@ -0,0 +1,13 @@
+# string functions 
+string1 = 'Aditya Magadum'
+print(type(string1))
+print(len(string1))
+
+
+# indexing
+# INDEXING - THE PROCESS OF FETCHING PERTICULAR CARECTOR IN PROGRAM
+a = 'VIRAT KOHLI'
+i = 0
+for i in a:
+    print(i,end='')
+    
