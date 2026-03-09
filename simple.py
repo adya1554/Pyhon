71d74e3b223c627a0b88b419edcd6234d2fcc957
aditya <aditya@gmail.com>
2026-03-09 11:13:01 +0530

    simple program to github

---
+++ simple.py
@@ -0,0 +1,11 @@
+# Simple Python Program
+
+# Print a greeting
+print("Hello, Looser! Welcome to Python.")
+
+# Do a basic calculation
+a = 5
+b = 3
+sum_result = a + b
+
+print("The sum of", a, "and", b, "is:", sum_result)
