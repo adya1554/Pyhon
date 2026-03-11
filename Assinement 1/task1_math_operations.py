4766b6d3a8ecff42add8b8f7768c9d0f556ef892
aditya <adityamagadum108@gmail.com>
2026-03-11 11:33:18 +0530

    adding assinement 1

---
+++ Assinement 1/task1_math_operations.py
@@ -0,0 +1,9 @@
+# taking user input 
+num1 = int(input('Enter 1st nmber of your choice - '))
+num2 = int(input('Enter 2nd number of your choice - '))
+
+# perform basic math operation of this numbers
+print('Addition : ',num1+num2)
+print('Substraction : ',num1-num2)
+print('Multiflication : ',num1*num2)
+print('Division : ',num1/num2)
---
+++ Assinement 1/task2_greeting.py
@@ -0,0 +1,9 @@
+# taking user input 
+fname = input('Enter your first Name -')
+lname = input('Enter your last name -')
+
+# concatinate fname anf lname into full_name
+full_name = fname+" "+lname
+
+# persnalized greeting massage using full name
+print(f"Hello, {full_name}! Welcome to python program.")
