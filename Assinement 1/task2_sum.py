50a65289ef41a30ea777a6f28274e3fefd691615
aditya <adityamagadum108@gmail.com>
2026-03-16 23:15:25 +0530

    commit

--- Assinement 1/task2_sum.py
+++
@@ -1,8 +0,0 @@
-x = 1
-sum = 0
-for i in x:
-    if i <= 50:
-        sum = sum + i
-        i++
-
-print(sum)
---
+++ conditional statements/forloop.py
@@ -0,0 +1,29 @@
+# # if else with example 
+# age = int(input('age enter properlyy : '))
+# if age >=18:
+#     print('BJP WINS')
+# else:
+#     print('CONGRESS LOST')
+
+
+# odd even checking
+
+# num = int(input('enter no of your choice :'))
+# if num % 2 ==0 :
+#     print('EVEN')
+# else:
+#     print('ODD')
+
+# students grade with marks
+
+marks = int(input('Enter your marks and we will tell your grade: '))
+if marks>=90:
+    print("you secured 'A+'")
+elif marks >= 80 and marks<90:
+    print("you secured 'A'")
+elif marks >=70  and marks<80:
+    print("you secured 'B'")
+elif marks >= 60 and marks< 70:
+    print("You secured 'C'")
+else:
+    print("you are waste BODY")
