6812e23cabf7118647b7e5d914981e23f3accc7d
aditya <adityamagadum108@gmail.com>
2026-03-17 22:44:24 +0530

    last

--- for loop/for.py
+++ for loop/for.py
@@ -8,3 +8,20 @@
 s1 = "Hello World"
 for i in s1:
     print(i)
+
+git = [12, 1, 2, 2, 3, 1, 1, 1, 34, -2, 0]
+high = 0
+for i in git:
+    if i > high:
+        high = i
+
+low = 0
+for l in git:
+    if l < low and l >= 0:
+        low = l
+
+
+print('Lowest git commits',low)
+print('highest git commits ',high)
+
+print(s1)
