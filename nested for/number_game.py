dd6e28f9ef12ad5a3983495c427fb5159b809c49
aditya <adityamagadum108@gmail.com>
2026-03-20 23:14:24 +0530

    update in game

--- nested for/number_game.py
+++ nested for/number_game.py
@@ -6,12 +6,13 @@ print('you have 10 Attempts\n\n\t-----Game Start---------')
 
 rand = random.randint(1,50)
 
+isguess = False
 for i in range(1,11):
     print(f"You have {11-i} left")
     guess = int(input("Enter your Guess : "))
     if guess == rand :
         print("Congrats Your Guess is correct !!\n")
-        print('The number was ',guess,'Game Over.\n\tThank You')
+        isguess = True
         break
     elif guess > rand: 
         print('Your Guess is Lower ! Try Again :)\n')
@@ -20,7 +21,9 @@ for i in range(1,11):
     else:
         print("BETTER LUCK NEXT TIME !!")
 
-
+if isguess == False:
+    print('Bad Luck!! you exasted all your attempts and coudent guess the number.')
+print(f"The secret number was {rand}. Game Over")
 
     
 
