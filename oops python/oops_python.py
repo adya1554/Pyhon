19f6deef64b690eb33de7019a273ce650f7921c2
aditya <aditya@gmail.com>
2026-06-23 08:33:00 +0530

    adding

--- oops python/oops_python.py
+++ oops python/oops_python.py
@@ -5,3 +5,22 @@ object = container
 """
 fruits = ["mango","orange","apple"]
 print(fruits)
+
+# Define a class
+class Car:
+    # Constructor method to initialize object attributes
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    # Method to display car details
+    def show_details(self):
+        print(f"Car Brand: {self.brand}, Model: {self.model}")
+
+# Create objects (instances of the class)
+car1 = Car("Toyota", "Corolla")
+car2 = Car("Honda", "Civic")
+
+# Call methods on objects
+car1.show_details()
+car2.show_details()
