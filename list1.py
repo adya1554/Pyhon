712d22a563f952a670a43928cda4afdedfa0eb1b
Aditya magadum <adityamagadum108@gmail.com>
2026-09-04 13:39:35 +0530

    Initialize and modify list1 with various operations
    Initialize list1 with integers and a string, modify it by appending, removing, extending, and popping elements, and print the results.

---
+++ list1.py
@@ -0,0 +1,12 @@
+list1 = [1, 2, 3, 4, 'Aditya']
+print(list1)
+list1.append('hii')
+list1.remove(2)
+list1.extend([2,3])
+print(list1)
+
+print(f"this is the final list: {list1}")
+
+print("list using After pop method")
+list1.pop()
+print(list1)
